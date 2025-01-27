0cfa7a04794b11567f979428ffb56cd2e1c730d7
Diamochang <diamochang@yeah.net>
2025-01-27 21:16:38 +0800

    hotfix: 去除公测标识 (1.0.0)

--- admin/views/nav.php
+++ admin/views/nav.php
@@ -4,7 +4,7 @@
             <a class="navbar-brand" href="../index.php">
                 <img src="../views/assets/logo_text.svg" height="25" class="d-inline-block align-text-bottom"
                     alt="十八桥社区">
-                论坛入站测试系统（公测）
+                论坛入站测试系统
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,7 +20,7 @@
                         <a class="nav-link" href="https://github.com/bridgeeighteen/b18-exam">源代码</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="/admin/">管理</a>
+                        <a class="nav-link active" href="/admin/">管理（开发中）</a>
                     </li>
                 </ul>
             </div>
@@ -29,6 +29,3 @@
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col">
-                <div class="alert alert-primary" role="alert">
-                    本系统处于公测阶段，如有任何 Bug，请通过源代码仓库创建 Issue 向我们反馈，感谢支持。
-                </div>
--- views/nav.php
+++ views/nav.php
@@ -4,7 +4,7 @@
             <a class="navbar-brand" href="index.php">
                 <img src="./views/assets/logo_text.svg" height="25" class="d-inline-block align-text-bottom"
                     alt="十八桥社区">
-                论坛入站测试系统（公测）
+                论坛入站测试系统
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,7 +20,7 @@
                         <a class="nav-link" href="https://github.com/bridgeeighteen/b18-exam">源代码</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/admin/" disabled>管理（开发中）</a>
+                        <a class="nav-link" href="#">管理（开发中）</a>
                     </li>
                 </ul>
             </div>
@@ -29,9 +29,6 @@
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col">
-                <div class="alert alert-primary" role="alert">
-                    本系统处于公测阶段，如有任何 Bug，请通过源代码仓库创建 Issue 向我们反馈，感谢支持。
-                </div>
 <?php
 if (isset($_GET["old_domain"]) && $_GET["old_domain"] == "1") {
     echo '                <div class="alert alert-primary" role="alert">"';
